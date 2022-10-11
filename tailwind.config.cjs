ecfdc4c27de44bea3aa43f9608d63d0cf7272157
Dev-Myles <dev-myles@Debian-Linux>
2022-10-10 22:44:31 -0500

    feat: starting build banner built

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                HindReg: ['HindSiliguri-Regular'],
+                HindLight: ['HindSiliguri-Light'],
+                PopBold: ['Poppins-ExtraBold'],
+                PopLight: ['Poppins-ExtraLight'],
+                PopReg: ['Poppins-Regular'],
+                RobLight: ['Roboto-Light'],
+            },
+        },
+    },
+
+    plugins: [],
 }
